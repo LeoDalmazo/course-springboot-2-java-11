d359b18e1a823da43666fe55a349364aa882c70b
LeoDalmazo <leo.dalmazo@hotmail.com>
2022-06-27 22:34:36 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/PersonResource.java
+++ src/main/java/com/educandoweb/course/resources/PersonResource.java
@@ -1,20 +1,33 @@
 package com.educandoweb.course.resources;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.Person;
+import com.educandoweb.course.services.PersonService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/Persons")
 public class PersonResource {
-
+	
+	@Autowired 
+	private PersonService service;
+	
 	@GetMapping
-	public ResponseEntity<Person> findAll() {
-		Person u = new Person(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Person>> findAll() {
+		List<Person> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Person> findById(@PathVariable Long id) {
+		Person obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
